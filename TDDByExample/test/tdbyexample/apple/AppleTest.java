9b7036f8dabc7a4cb9fee34118bc5175e56ae91b
Nathan <nvaidyan@cpcom102macx01.care.asu.edu>
2012-04-01 15:36:31 -0700

    Post AppleTest, Pre Chapter 2

--- TDDByExample/test/tdbyexample/apple/AppleTest.java
+++ TDDByExample/test/tdbyexample/apple/AppleTest.java
@@ -19,6 +19,13 @@ public class AppleTest {
         Computer machine1 = new Computer("Apple");
         assertEquals("Apple", machine1.brand);
     }
+    
+    @Test
+    public void nothertestBrand(){
+        
+        Computer machine2 = new Computer("Windows");
+        assertEquals("Windows", machine2.brand);
+    }
 }
            
                             

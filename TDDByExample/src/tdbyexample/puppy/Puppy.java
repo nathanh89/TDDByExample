a256067e40fb8db0bf699a65421e68e9f049280e
Nathan <nvaidyan@cpcom102macx01.care.asu.edu>
2012-04-01 15:27:10 -0700

    Post Puppy Test, Pre Chapter 2

--- TDDByExample/src/tdbyexample/puppy/Puppy.java
+++ TDDByExample/src/tdbyexample/puppy/Puppy.java
@@ -1,16 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package tdbyexample.puppy;
 
-/**
- *
- * @author nvaidyan
- */
 public class Puppy {
     String emotion;
-    int wagState;
     
     Puppy(String emotion){
         this.emotion= emotion;

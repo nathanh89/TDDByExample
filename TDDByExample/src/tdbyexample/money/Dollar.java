91287111ae2d2806cbee0c1307a4486a03f89ccf
Nathan <nvaidyan@cpcom102macx01.care.asu.edu>
2012-04-01 17:36:15 -0700

    Post Chapter 2 for Nathan

--- TDDByExample/src/tdbyexample/money/Dollar.java
+++ TDDByExample/src/tdbyexample/money/Dollar.java
@@ -11,5 +11,7 @@ package tdbyexample.money;
 public class Dollar {
     int amount;
     Dollar(int amount){this.amount= amount;}
-    void times(int multiplier) {amount*= multiplier;}
+    Dollar times(int multiplier) {
+    return new Dollar(amount*multiplier);
+    }
 }
--- TDDByExample/test/tdbyexample/money/MoneyTest.java
+++ TDDByExample/test/tdbyexample/money/MoneyTest.java
@@ -7,14 +7,10 @@ public class MoneyTest {
      @Test
      public void testMultiplication() {
          Dollar five= new Dollar(5);
-         five.times(2);
-         assertEquals(10, five.amount);
+         Dollar product= five.times(2);
+         assertEquals(10, product.amount);
+         product= five.times(3);
+         assertEquals(15, product.amount);
      }
      
-     @Test
-     public void anotherMultiplicationTest(){
-         Dollar ducat= new Dollar(7);
-         ducat.times(8);
-         assertEquals(56, ducat.amount);
-     }
 }
